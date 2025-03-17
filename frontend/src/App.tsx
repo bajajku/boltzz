95a2815bb8bdab9ede191e69e829be6f48023d25
bajajku <bajajku@sheridancollege.ca>
2025-03-17 12:19:27 -0400

    fixed frontend dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
-// import { Builder } from './pages/Builder';
-// import { parseXml } from './steps';
+import { Builder } from './pages/Builder';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/builder" element={<Builder />} /> */}
+        <Route path="/builder" element={<Builder />} />
       </Routes>
     </BrowserRouter>
   );
